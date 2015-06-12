ed28f2a7d34a5c6697505998772a3f3c7f1751e5
michael febrianto <michaelfebrianto@gmail.com>
2015-06-13 07:04:47 +1000

    master : try upload file

--- app/scripts/controllers/admins/food.js
+++ app/scripts/controllers/admins/food.js
@@ -8,10 +8,59 @@
  * Controller of the fancybiteApp
  */
 angular.module('fancybiteApp')
-  .controller('AdminsFoodCtrl', function ($scope) {
+
+  .directive('fileModel', ['$parse', function ($parse) {
+    return {
+      restrict: 'A',
+      link: function(scope, element, attrs) {
+        var model = $parse(attrs.fileModel);
+        var modelSetter = model.assign;
+
+        element.bind('change', function(){
+          scope.$apply(function(){
+            modelSetter(scope, element[0].files[0]);
+          });
+        });
+      }
+    };
+  }])
+
+  .service('fileUpload', ['$http', function ($http) {
+    this.uploadFileToUrl = function(file, uploadUrl){
+      var fd = new FormData();
+      fd.append('food_image', file);
+      $http.post(uploadUrl, fd, {
+        transformRequest: angular.identity,
+        headers: {'Content-Type': undefined}
+      })
+        .success(function(){
+        })
+        .error(function(){
+        });
+    }
+  }])
+
+  .controller('AdminsFoodCtrl', function ($scope, fileUpload) {
     $scope.awesomeThings = [
       'HTML5 Boilerplate',
       'AngularJS',
       'Karma'
     ];
+
+    $scope.uploadFile = function(){
+      var file = $scope.myFile;
+      console.log('file is ' + JSON.stringify(file));
+      var uploadUrl = "/menubook/foods/upload_image";
+      fileUpload.uploadFileToUrl(file, uploadUrl);
+    };
+
+    $scope.initForm = function(){
+      $scope.food = {
+        name: '',
+        description: ''
+      };
+    };
+
+    $scope.initForm();
+
   });
